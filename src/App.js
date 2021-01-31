44387323170d19337604ea030be2eb74280d221b
Bo Geum Kim <epdlsemgks872@gmail.com>
2021-01-31 19:48:45 +0900

    Add 적재 불량 차량 적발 앱 디자인

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Busted from './components/Busted';
+
+class App extends Component {
+  render() {
+    return (
+      <Busted>
+        템플릿
+      </Busted>
+    );
+  }
+}
+
+export default App;
