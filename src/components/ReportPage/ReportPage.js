ee5a63121b153804efe9618c64d934e9fd15ca59
Bo Geum Kim <epdlsemgks872@gmail.com>
2021-02-08 19:29:05 +0900

    PR TEST

--- src/components/ReportPage/ReportPage.js
+++ src/components/ReportPage/ReportPage.js
@@ -25,7 +25,7 @@ class ReportPage extends Component {
               <button>촬영 / 갤러리</button>
             </div>
             <div className="image-wrapper">
-              차량 이미지 보일 공간
+              차량 이미지 보일 공간ddd
             </div>
             <div className="accident-spot">* 발생지역</div>
             <div className="spot-wrapper">
