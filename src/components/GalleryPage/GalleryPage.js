9fd1a117ef67b768fdcef982163d75ccba0b5e00
Bo Geum Kim <epdlsemgks872@gmail.com>
2021-02-18 10:39:59 +0900

    Update GalleryPage

--- src/components/GalleryPage/GalleryPage.js
+++ src/components/GalleryPage/GalleryPage.js
@@ -1,6 +1,6 @@
 import React, { Component }  from "react";
 import { withRouter } from "react-router-dom";
-import axios from 'axios';
+import ImageUploader from "react-images-upload";
 
 import "./GalleryPage.css";
 
@@ -8,10 +8,8 @@ class GalleryPage extends Component {
   constructor(props) {
     super(props);
     this.goToPage = this.goToPage.bind(this);
-    this.state = {
-      file : '',
-      previewURL : ''
-    }
+    this.state = { pictures: [] };
+    this.onDrop = this.onDrop.bind(this);
   };
   
   goToPage = (page) =>  {
@@ -21,47 +19,14 @@ class GalleryPage extends Component {
   goBack = (page) =>  {
     this.props.history.goBack(page);
   };
-
-  state = { selectedFiles: null };
   
-  onClickHandler = event => {
-    const formData = new FormData();
-    
-    formData.append(
-      "uploadImages",
-      this.state.selectedFiles,
-      this.state.selectedFiles.name
-    );
-
-    const config = {
-      headers: {
-        "content-type": "multipart/form-data"
-      }
-    };
-    
-    axios.post(`uploadAPI`, formData, config);
-  };
-
-  fileChangedHandler = event => {
-    event.preventDefault();
-    let reader = new FileReader();
-    let file = event.target.files[0];
-    reader.onloadend = () => {
-      this.setState({
-        file : file,
-        previewURL : reader.result
-      })
-    }
-    reader.readAsDataURL(file);
+  onDrop(pictureFiles, pictureDataURLs) {
+    this.setState({
+      pictures: this.state.pictures.concat(pictureFiles)
+    });
   };
 
   render() {
-    let img_preview = null;
-    
-    if(this.state.file !== ''){
-      img_preview = <img className='img_preview' src={this.state.previewURL}></img>
-    }
-
     return (
       <div>
         <main className="busted-template">
@@ -72,11 +37,15 @@ class GalleryPage extends Component {
           </div>
 
           <div className="body-wrapper">
-            <div className="image-wrapper">
-              <label for="select-image" className="select-image-btn">+</label>
-              <input type="file" accept="image/*" id="select-image" onChange={this.fileChangedHandler}></input>
-              {img_preview}
-            </div>
+            <ImageUploader
+              withIcon={true}
+              buttonText='이미지를 선택하세요.'
+              label="최대 사진 크기: 5MB, jpg/gif/png 형식만 가능"
+              onChange={this.onDrop}
+              imgExtension={['.jpg', '.gif', '.png', '.gif']}
+              maxFileSize={5242880}
+              withPreview={true}
+            />
           </div>
 
           <div className="footer">
